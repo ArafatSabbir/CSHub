2b662d60489515322ce5842ef8d587c7ed636e98
ArafatSabbir <arafatsabbir007@gmail.com>
2019-03-11 12:10:03 +0600

    Initial commit

---
+++ app/src/main/java/com/sslabs/sabbir/cshub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sslabs.sabbir.cshub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
